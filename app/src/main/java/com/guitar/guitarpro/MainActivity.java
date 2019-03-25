a2361affe576fb9f61fd9f6a8477c13c7e98fbd5
Renáta <renatandreea@gmail.com>
2019-03-25 10:27:25 +0200

    BottomNavigation with Fragments

--- app/src/main/java/com/guitar/guitarpro/MainActivity.java
+++ app/src/main/java/com/guitar/guitarpro/MainActivity.java
@@ -1,15 +1,60 @@
 package com.guitar.guitarpro;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        loadFragment(new SettingsFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.chords:
+                fragment = new ChordsFragment();
+                break;
+
+            case R.id.scales:
+                fragment = new ScalesFragment();
+                break;
+
+            case R.id.songs:
+                fragment = new SongsFragment();
+                break;
+
+            case R.id.settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
