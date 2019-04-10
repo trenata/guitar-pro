be6626ba9fb8155b360b5eee2b8d99bf6530b0cf
Zsolt Boldizsár <zsolt@halcyonmobile.com>
2019-04-10 14:11:08 +0300

    Remove duplicate classes

--- app/src/main/java/com/guitar/guitarpro/SettingsFragment.java
+++ app/src/main/java/com/guitar/guitarpro/SettingsFragment.java
@@ -1,10 +1,13 @@
 package com.guitar.guitarpro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -16,7 +19,17 @@ public class SettingsFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, null);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        Button button = view.findViewById(R.id.see_onboarding);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),
+                        OnboardingActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
