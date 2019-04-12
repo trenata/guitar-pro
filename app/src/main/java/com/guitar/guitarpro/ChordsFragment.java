a351c959ab16c8dfbcca4939e8a78014b62840f1
Renáta <renatandreea@gmail.com>
2019-04-12 20:59:04 +0300

    CheckedTextView listener (not working)

--- app/src/main/java/com/guitar/guitarpro/ChordsFragment.java
+++ app/src/main/java/com/guitar/guitarpro/ChordsFragment.java
@@ -1,9 +1,12 @@
 package com.guitar.guitarpro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckedTextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,6 +20,14 @@ public class ChordsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        CheckedTextView checkedTextView = getView().findViewById(R.id.chordC);
+        checkedTextView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // do sthg
+            }
+        });
+
         return inflater.inflate(R.layout.chords_fragment, null);
     }
 }
