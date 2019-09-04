4f3bad5c403370e6d3ae09e78a9bc6712691403a
Renáta <renatandreea@gmail.com>
2019-09-04 23:37:30 +0300

    Fix Hm chord to be in a line

--- app/src/main/java/com/guitar/guitarpro/ChordsFragment.java
+++ app/src/main/java/com/guitar/guitarpro/ChordsFragment.java
@@ -6,18 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckedTextView;
 
+import com.guitar.guitarpro.connectivity.RealGuitarConnectionManager;
+import com.guitar.guitarpro.model.Chord;
+import com.guitar.guitarpro.model.Color;
+import com.guitar.guitarpro.model.Key;
+import com.guitar.guitarpro.model.Note;
+import com.guitar.guitarpro.model.SelectableChord;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.guitar.guitarpro.connectivity.RealGuitarConnectionManager;
-import com.guitar.guitarpro.model.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ChordsFragment extends Fragment implements ChordsAdapter.OnItemSelectedListener {
 
